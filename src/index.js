4d589bedc879defb009d1a21cc1febf71a21fc1d
matthewaubert <132402803+matthewaubert@users.noreply.github.com>
2023-11-06 18:09:09 -0500

    Write function to hit weather API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 // fetch data from weather API; take location as input
 async function fetchWeather(location) {
+  const apiKey = 'ab36ae6f835d44c781e191306230411';
+
   const response = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=ab36ae6f835d44c781e191306230411&q=${location}`
+    `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${location}&days=3&alerts=yes`
   );
   const weatherData = await response.json();
-  console.log(weatherData);
+  // HANDLE ERRORS!
+
+  return weatherData;
 }
 
-fetchWeather();
+
+// fetchWeather('philadelphia', 'current').then(res => console.log(res));
+fetchWeather('philadelphia').then(res => console.log(res));
